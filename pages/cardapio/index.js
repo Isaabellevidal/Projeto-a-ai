70297e3c016524696aa60b59446dda1843140c87
Isaabellevidal <166124378+Isaabellevidal@users.noreply.github.com>
2024-08-26 10:53:51 -0300

    Atualizado
    Atualizado

--- pages/cardapio/index.js
+++ pages/cardapio/index.js
@@ -1,15 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image} from 'react-native';
+import {  useFonts, Poppins_400Regular } from '@expo-google-fonts/poppins';
 
-export default function Cardapio(){
-    return (
+const Icon = require('../../assets/img/acai-inicial.png');
+const Icon2 = require('../../assets/img/tigela-1-verd.png');
+const Icon3 = require('../../assets/img/tigela-5-roxo.png');
+const Icon4 = require('../../assets/img/tigela-2-verde.png');
+const Icon5 = require('../../assets/img/copo-2-roxo.png');
+const Icon6 = require('../../assets/img/copo-3-verde.png');
+const Icon7 = require('../../assets/img/copo-6-roxo.png');
+
+
+export default function Cardapio() {
+  let [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+
+
+      // <ScrollView>
         <View style={styles.container}>
-        <Text>Cardapio</Text>
-        <StatusBar style="auto" />
-      </View>
+
+        <Text style={styles.title}>ESCOLHA JÁ O </Text>
+        <Text style={styles.titlee}>SEU AÇAI</Text>
+
+        <Text style={styles.subtitle}>VEJA AS OPÇÕES ABAIXO:</Text>
+
+
+        <View style={styles.imgContainer}>
+        <TouchableOpacity >
+        <Image source={Icon7} style={styles.imgg} />
+       </TouchableOpacity>
+
+       <Text style={styles.subtitleimg}>AÇAI COM CONFETE:</Text>
+       <Text style={styles.paraph}>hh:</Text>
+       </View>
+
+       <View style={styles.imgContainer}>
+        <TouchableOpacity >
+        <Image source={Icon6} style={styles.imgg} />
+       </TouchableOpacity>
+
+       <Text style={styles.subtitleimg}>VEJA AS OPÇÕES ABAIXO:</Text>
+       </View>
+
+
+       <View style={styles.imgContainer}>
+        <TouchableOpacity >
+        <Image source={Icon5} style={styles.imgg} />
+       </TouchableOpacity>
+
+       <Text style={styles.subtitleimg}>VEJA AS OPÇÕES ABAIXO:</Text>
+       </View>
+
+
+
+
+
+
+
+
+
+
+        </View>
+
+        // </ScrollView>
+
+ 
     );
   }
   
+
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -17,4 +83,55 @@ export default function Cardapio(){
       alignItems: 'center',
       justifyContent: 'center',
     },
+    title:{
+      fontSize: 28,
+      color: '#65146d',
+      right:80,
+      fontFamily:'Poppins_800ExtraBold',
+    },
+    titlee:{
+      fontSize: 28,
+      color: '#9fcd2c',
+      right:120,
+      fontFamily:'Poppins_800ExtraBold',
+    },
+    subtitle:{
+      fontSize: 15,
+      color: '#65146d',
+      textAlign: 'center',
+      right:85,
+      fontFamily:'Poppins_800ExtraBold',
+    },
+    subtitleimg:{
+      fontSize: 15,
+      color: '#65146d',
+      textAlign: 'left',
+    marginBottom:50,
+      fontFamily:'Poppins_800ExtraBold',
+    },
+
+    imgg: {
+      top:20,
+      width: 130,
+      height:160,
+      resizeMode: 'contain', 
+      marginHorizontal: 10, 
+      right:20,
+      marginBottom:15,
+    },
+  
+    imgContainer: {
+      flexDirection: 'row', 
+      alignItems: 'center',// Organiza os itens em linha horizontal
+    }, 
+    paraph:{
+      fontSize: 10,
+      color: '#65146d',
+      textAlign: 'left',
+      marginBottom:10,
+      fontFamily:'Poppins_400Regular',
+
+
+    },
+
   });
--- pages/local/index.js
+++ pages/local/index.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+
+
+const Icon = require('../../assets/img/local-img.png');
+
 
 export default function Local(){
     return (
         <View style={styles.container}>
-        <Text>Local</Text>
-        <StatusBar style="auto" />
+        <Text  style={styles.title}>Já conhece </Text>
+        <Text  style={styles.title}> a nossa loja?</Text>
+
+        <Image source={Icon} style={styles.img} />
+
+        <Text  style={styles.titlee}>Venha conhecer nossa loja</Text>
+
       </View>
     );
   }
@@ -17,4 +27,23 @@ export default function Local(){
       alignItems: 'center',
       justifyContent: 'center',
     },
+    title:{
+      fontSize: 28,
+      color: '#65146d',
+      right:80,
+      fontFamily:'Poppins_800ExtraBold',
+    },
+    titlee:{
+      fontSize: 25,
+      color: '#65146d',
+      fontFamily:'Poppins_800ExtraBold',
+    },
+  img: {
+
+    width: 415,
+    height:490,
+    resizeMode: 'contain', 
+    borderColor: 'red'
+  },
+
   });
